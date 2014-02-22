8aab15f253187694cbcc16a74948f49aa1c01a20
wahlker <anthony.santostefano@gmail.com>
2014-02-22 00:02:50 -0500

    Lab 5 broken stuff going to come back to it this weekend.

---
+++ laboutput5.sql
@@ -0,0 +1,36 @@
+﻿SELECT Agents.city
+ FROM Agents, Customers, Orders
+ WHERE Customers.name = 'Basics' AND Orders.cid = Customers.cid;
+
+SELECT Orders.pid, Customers.city
+ FROM Customers, Orders
+ WHERE Customers.city = 'Kyoto' AND Orders.cid = Customers.cid;
+
+SELECT Customers
+ FROM Customers
+ WHERE cid NOT IN (
+	Select cid
+	FROM Orders
+	Where cid IS NOT NULL)
+
+SELECT *
+ FROM Customers LEFT OUTER JOIN Orders ON (Customers.cid = Orders.cid)
+ WHERE Orders.cid IS NULL;
+
+SELECT Customers.name, Agents.name
+ FROM Customers LEFT OUTER JOIN Agents ON (Customers.city = Agents.city)
+ WHERE cid IN (
+	SELECT cid
+	From Orders
+	WHERE cid is NOT NULL)
+			  
+SELECT Customers.name, Agents.name
+ FROM Customers LEFT OUTER JOIN Agents ON (Customers.city = Agents.city)
+ WHERE Agents.city = Customers.city;
+
+SELECT min(Products.quantity)
+ FROM Customers LEFT OUTER JOIN Products ON (Customers.city = Products.city);
+ 
+
+
+ 
