53b406576124b0ab956664c05c61dce770034495
wahlker <anthony.santostefano@gmail.com>
2014-02-28 12:25:07 -0800

    Update lab6.sql

--- lab6.sql
+++ lab6.sql
@@ -17,3 +17,10 @@ SELECT Customers.name, Orders.pid, Orders.dollars
  FROM Customers LEFT OUTER JOIN Orders ON (Customers.cid = Orders.cid)
  WHERE Orders.dollars IS NOT NULL
  ORDER BY Orders.dollars DESC
+ 
+SELECT Customers.name, Products.name, Agents.name
+ FROM Customers LEFT OUTER JOIN Products ON (Customers.city = Products.city)
+ INNER JOIN Agents ON (Agents.city = Customers.city)
+ WHERE Products.name IS NOT NULL
+
+// 6 is wrong still working.
